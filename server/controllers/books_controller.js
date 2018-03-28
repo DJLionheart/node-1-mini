93be26c62e1fbe7dcf8a3084202b07245942fdb0
Paul Peterson <paulpeterson@Pauls-MacBook-Pro.local>
2018-03-28 14:41:15 -0600

    update

--- server/controllers/books_controller.js
+++ server/controllers/books_controller.js
@@ -1,1 +1,53 @@
-var books = [];
+let books = [];
+let id = 0;
+
+module.exports = {
+    read: (req, res) => {
+        res.status(200).send(books);
+        console.log('The required tomes hath been sent...');
+        
+    },
+
+    create: (req, res) => {
+        let book = {
+            title: req.body.title,
+            author: req.body.author,
+            id: id
+        }
+        books.push(book);
+        id++;
+        res.status(200).send(books);
+        console.log('A tome hath been added to thine library...');
+        
+    },
+
+    update: ( req, res ) => {
+        
+        console.log(books);
+        let index = null;
+        books.forEach((book, i) => {
+          if(book.id === +req.params.id) index = i;
+          
+        })
+        
+        books[ index ] = {
+          id: req.params.id,
+          title: req.body.title || books[ index ].title,
+          author: req.body.author || books[ index ].author
+        };
+        res.status(200).send( books );
+        console.log('The requested tome hath been updated...');
+        
+      },
+
+      delete: (req, res) => {
+          let index = null;
+          let { id } = req.params;
+          books.forEach( (book, i) => {
+              if(book.id === +id ) index = i;
+          });
+          books.splice(index, 1);
+          res.status(200).send(books);
+          console.log('The requested tome hath been removed...');
+      }
+}
--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const ctrl = require('./controllers/books_controller')
+const bc = require('./controllers/books_controller')
 
 const app = express();
-
-
 app.use(bodyParser.json());
 
 
+app.get('/api/books', bc.read);
 
+app.post('/api/books', bc.create);
 
+app.put('/api/books/:id', bc.update);
+
+app.delete('/api/books/:id', bc.delete)
 
 
 
